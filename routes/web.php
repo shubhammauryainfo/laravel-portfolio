5eecc2acf7ab8375abce0d5715d937fb8c668011
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-03-02 23:44:42 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/intro', function () {
+    return view('intro');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
