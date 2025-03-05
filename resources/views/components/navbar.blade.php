c0c84f0e019e16effa8fd35ad5e55f712484cfe5
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-03-05 23:26:04 +0530

    update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded shadow-md">
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-1 rounded shadow-md">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
         <!-- Logo -->
         <a href="/" class="flex items-center">
@@ -17,7 +17,7 @@
         <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
             <ul class="flex flex-col md:flex-row md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
                 <li>
-                    <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 hover:text-blue-700 md:p-0">Home</a>
+                    <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 hover:text-blue-700  hover:border-b-4 hover:border-blue-600 md:p-4">Home</a>
                 </li>
                 <li>
                     <a href="/about" class="block py-2 pr-4 pl-3 text-gray-700 hover:text-blue-700 md:p-0">About</a>
